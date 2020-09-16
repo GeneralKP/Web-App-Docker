564cecc87993963ff74965f0b1e86c5948d612d3
GeneralKP <kevin.perez@unac.edu.co>
2020-09-15 20:26:45 -0500

    Debugger config and requirements.txt added

---
+++ users/project/config.py
@@ -0,0 +1,19 @@
+class BaseConfig:
+    """Configuración base"""
+
+    TESTING = False
+
+class DevelopmentConfig(BaseConfig):
+    """Configuración de desarrollo"""
+
+    pass
+
+class TestingConfig(BaseConfig):
+    """Configuración de Testing"""
+
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    """Configuración de producción"""
+
+    pass
