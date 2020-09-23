e1280fe8be9b0a467e31861abad66422f9f5f0ff
GeneralKP <kevin.perez@unac.edu.co>
2020-09-22 22:36:27 -0500

    Server fully owrking. Tests failed

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,19 +1,24 @@
-class BaseConfig:
-    """Configuración base"""
+import os
 
+class BaseConfig:
+    """Configuracion base"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_key'  # nuevo
 
 class DevelopmentConfig(BaseConfig):
     """Configuración de desarrollo"""
 
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # nuevo
 
 class TestingConfig(BaseConfig):
     """Configuración de Testing"""
 
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")  # nuevo
+
 
 class ProductionConfig(BaseConfig):
     """Configuración de producción"""
 
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # nuevo
